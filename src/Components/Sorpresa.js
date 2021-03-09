542d8af6aedc9c031902b8deddc782a39dfef32e
wasantac <34143200+wasantac@users.noreply.github.com>
2021-03-08 23:16:53 -0500

    cheems chiqui

--- src/Components/Sorpresa.js
+++ src/Components/Sorpresa.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../css/sorpresa.css'
+import {Row,Container,Col} from 'reactstrap';
 const Sorpresa = () => {
     let [show,setShow] = useState(true);
     let audio = new Audio("2 UNLIMITED - Get Ready For This (Official Music Video).mp3");
@@ -28,11 +29,25 @@ const Sorpresa = () => {
                 setShow(!show)
                 audio.play();
             }}>Confía bb</button> : 
-            <div>    
+            <Container>
+            <Row className="align-items-center justify-content-center">  
+                <Col className="d-flex justify-content-center">
+                <img src={process.env.PUBLIC_URL + '/cheems.png'} alt="cheems" width="auto" height="200px"
+                className="cheemsPeque"></img>               
+                </Col>  
+
+                <Col className="d-flex justify-content-center">                
                 <img src={process.env.PUBLIC_URL + '/cheems.png'} alt="cheems" width="auto" height="400px"
                 className="cheems"></img>
-            </div>}
+                </Col>
+                <Col className="d-flex justify-content-center">                
+                <img src={process.env.PUBLIC_URL + '/cheems.png'} alt="cheems" width="auto" height="200px"
+                className="cheemsPeque"></img>
+                </Col>
+            </Row>
+            </Container>}
         </div>
+
     );
 }
 
